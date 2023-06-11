09b9facadfd7f0bb2bba11e08f0d1bf061f2bfca
FabioTrindade <fabio.trindade@hotmail.com>
2023-06-11 12:11:39 +0100

    Increase unit test

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTest.cs
@@ -0,0 +1,19 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests;
+
+[TestClass]
+public class TodoItemTests
+{
+
+    private readonly TodoItem _validTodo = new("Titulo tarefa", DateTime.Now, "Fábio Trindade");
+
+    [TestMethod]
+    public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+    {
+
+        Assert.AreEqual(_validTodo.Done, false);
+    }
+}
---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,38 @@
+using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Entities;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers;
+
+public class TodoHandler :
+    Notifiable<Notification>,
+    IHandler<CreateTodoCommand>
+{
+    private readonly ITodoRepository _repository;
+
+    public TodoHandler(ITodoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public ICommandResult Handler(CreateTodoCommand command)
+    {
+        command.Validate();
+
+        if(!command.IsValid)
+            return new GenericCommandResult(
+                false,
+                "Ops, parece que sua tarefa está errada!",
+                command.Notifications
+            );
+
+        var todo = new TodoItem(command.Title, command.Date, command.User);
+        
+        _repository.Create(todo);
+
+        return new GenericCommandResult(true, "Tarefa salva.", todo);
+    }
+}
