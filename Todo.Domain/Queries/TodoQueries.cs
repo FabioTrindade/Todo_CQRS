b64b4563f4ce525fd665411eb439c1306b7a2e4e
FabioTrindade <fabio.trindade@hotmail.com>
2023-06-12 20:10:22 +0100

    Added query in project

---
+++ Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,24 @@
+
+using System.Linq.Expressions;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries;
+
+public static class TodoQueries
+{
+    public static Expression<Func<TodoItem, bool>> GetAll(string user)
+        =>  t => t.User == user;
+    
+    public static Expression<Func<TodoItem, bool>> GetAllDone(string user)
+        => t => t.User == user && t.Done;
+    
+    public static Expression<Func<TodoItem, bool>> GetAllUndone(string user)
+        => t => t.User == user && !t.Done;
+
+    public static Expression<Func<TodoItem, bool>> GetPeriod(string user, DateTime date, bool done)
+        =>  t => 
+            t.User == user &&
+            t.Done == done &&
+            t.Date.Date == date.Date;
+    
+}
