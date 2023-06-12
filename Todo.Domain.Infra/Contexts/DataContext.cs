cbae19bd66790de1d8ad4e0059c886e2ba7b1002
FabioTrindade <fabio.trindade@hotmail.com>
2023-06-12 12:34:14 +0100

    Added column type

--- Todo.Domain.Infra/Contexts/DataContext.cs
+++ Todo.Domain.Infra/Contexts/DataContext.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using Todo.Domain.Entities;
 
@@ -8,5 +7,16 @@ public class DataContext : DbContext
     {
     }
 
-    public DbSet<TodoItem> Todos { get; set;}
-} 
+    public DbSet<TodoItem> Todos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<TodoItem>().ToTable("Todo");
+        builder.Entity<TodoItem>().Property(p => p.Id);
+        builder.Entity<TodoItem>().Property(p => p.User).HasMaxLength(120).HasColumnType("varchar(120)");
+        builder.Entity<TodoItem>().Property(p => p.Title).HasMaxLength(160).HasColumnType("varchar(160)");
+        builder.Entity<TodoItem>().Property(p => p.Done).HasColumnType("bit");
+        builder.Entity<TodoItem>().Property(p => p.Date);
+        builder.Entity<TodoItem>().HasIndex(i => i.User);
+    }
+}
